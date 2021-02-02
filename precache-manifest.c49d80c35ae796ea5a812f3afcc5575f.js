568b919299d4950f7603a88d9ad0b48ff6d68a4f
akshata8308 <akshatashinde4628@gmail.com>
2021-02-03 00:18:56 +0530

    Updates

---
+++ precache-manifest.c49d80c35ae796ea5a812f3afcc5575f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47f69e3ee498f4f3d6d509661c60a3a7",
+    "url": "/Chargebee/index.html"
+  },
+  {
+    "revision": "c23c07219bec014eeb02",
+    "url": "/Chargebee/static/css/main.a7e35919.chunk.css"
+  },
+  {
+    "revision": "c3445a800e4a2457bc2b",
+    "url": "/Chargebee/static/js/2.ebc7d456.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Chargebee/static/js/2.ebc7d456.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c23c07219bec014eeb02",
+    "url": "/Chargebee/static/js/main.1b64667b.chunk.js"
+  },
+  {
+    "revision": "e746286698d3799d17a6",
+    "url": "/Chargebee/static/js/runtime-main.8ce0c0fe.js"
+  },
+  {
+    "revision": "eafd49b9b98e0b63a49c0b004e3ebf9a",
+    "url": "/Chargebee/static/media/acc-logos.eafd49b9.png"
+  },
+  {
+    "revision": "d2d9d680a8e98eb8a868df8d090a1f6c",
+    "url": "/Chargebee/static/media/chargebee.d2d9d680.png"
+  },
+  {
+    "revision": "78fe49d4ebd87c4a3d31015ea8c54516",
+    "url": "/Chargebee/static/media/crm-logos.78fe49d4.png"
+  },
+  {
+    "revision": "e9e4e361e4ef89d786f1b88bf59f4937",
+    "url": "/Chargebee/static/media/recurring-billing-graphic.e9e4e361.svg"
+  },
+  {
+    "revision": "cc8018210447432f755ba9642d41776d",
+    "url": "/Chargebee/static/media/revenue-management.cc801821.svg"
+  },
+  {
+    "revision": "1c93155467f6614913b7f1a0fb292d8c",
+    "url": "/Chargebee/static/media/revenue-operations-graphic.1c931554.svg"
+  },
+  {
+    "revision": "1d1d54fb853877397804c5534ab5b52e",
+    "url": "/Chargebee/static/media/subscription-graphic.1d1d54fb.svg"
+  }
+]);
